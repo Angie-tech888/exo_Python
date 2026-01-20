4fdf3054d3d3268130d22abd97b2a1d8edd85f40
Angélia Teodosio <2004crt@gmail.com>
2026-01-20 18:20:56 +0100

    Add print statements for rounding and formatting examples

---
+++ 1.7p27.py
@@ -0,0 +1,10 @@
+#question d'affiche quaffiche chacune des instruction suivante l'orsque execute
+print(round(10 + 1/3, 3))
+#affiche 10.333
+print(format(10 + 1/3, ".3g"))
+#affiche 10.3
+print(round(2/3, 3))
+#affiche 0.667
+print("dis", "continu")
+#affiche dis continu
+#fin exercice 7
