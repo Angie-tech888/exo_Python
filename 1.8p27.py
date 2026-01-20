cf40fbc569a43417cc951f05be35623a7f04ebe8
Angélia Teodosio <2004crt@gmail.com>
2026-01-20 18:31:06 +0100

    Create 1.8p27.py

---
+++ 1.8p27.py
@@ -0,0 +1,10 @@
+#arrondissement significatif quaffiche chacune des instruction suivantes l'orsque on l'execute
+print(format(2469.75103, ".5g"))
+#affiche 2469.8
+print(format(2469.75103, ".4g"))
+#affiche 2470
+print(format(2469.75103, ".3g"))
+#affiche 2.47e+03
+print(format(2469.75103, ".2g"))
+#affiche 2.5e+03
+#fin exercice 8
